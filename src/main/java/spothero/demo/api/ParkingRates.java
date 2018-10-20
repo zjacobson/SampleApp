2b1f710e71031cda3e7a01eead6a7cd063ce8646
Zak Jacobson <zak@curbyourgod.com>
2018-10-20 16:52:23 -0500

    ParkingRates from JSON

--- src/main/java/spothero/demo/api/ParkingRates.java
+++ src/main/java/spothero/demo/api/ParkingRates.java
@@ -1,5 +1,6 @@
 package spothero.demo.api;
 
+import com.google.gson.GsonBuilder;
 import lombok.Data;
 
 import java.util.List;
@@ -7,18 +8,52 @@ import java.util.List;
 @Data
 class ParkingRates {
     static ParkingRates testData() {
-        return new ParkingRates();
+        return  new GsonBuilder().create().fromJson(ParkingRates.testData, ParkingRates.class);
     }
+
 
     List<Rate> rates;
 
-    public boolean hasRates() {
-        return false;
+    boolean hasRates() {
+        return rates != null && !rates.isEmpty();
     }
 
     static class Rate {
+
         String days;
         String times;
         String price;
     }
+
+
+
+    static String testData = "{" +
+            "    \"rates\": [" +
+            "        {" +
+            "            \"days\": \"mon,tues,thurs\"," +
+            "            \"times\": \"0900-2100\"," +
+            "            \"price\": 1500" +
+            "        }," +
+            "        {" +
+            "            \"days\": \"fri,sat,sun\"," +
+            "            \"times\": \"0900-2100\"," +
+            "            \"price\": 2000" +
+            "        }," +
+            "        {" +
+            "            \"days\": \"wed\"," +
+            "            \"times\": \"0600-1800\"," +
+            "            \"price\": 1750" +
+            "        }," +
+            "        {" +
+            "            \"days\": \"mon,wed,sat\"," +
+            "            \"times\": \"0100-0500\"," +
+            "            \"price\": 1000" +
+            "        }," +
+            "        {" +
+            "            \"days\": \"sun,tues\"," +
+            "            \"times\": \"0100-0700\"," +
+            "            \"price\": 925" +
+            "        }" +
+            "    ]" +
+            "}";
 }
